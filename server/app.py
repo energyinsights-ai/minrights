7bcc57b8fb410d613b1f0f92826156aac05f1b92
Bill Givens <bg@dde.ai>
2024-06-27 11:26:15 -0500

    update

--- server/app.py
+++ server/app.py
@@ -16,7 +16,7 @@ import datetime
 
 load_dotenv()
 # instantiate the app
-app = Flask(__name__)
+app = Flask(__name__,static_folder='static')
 app.config.from_object(__name__)
 app.config.update(SECRET_KEY=os.getenv("SECRET_KEY"))
 app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql+psycopg2://{os.getenv('DB_USER')}:{os.getenv('DB_PWD')}@{os.getenv('DB_HOST')}:5432/{os.getenv('DB_NAME')}"
